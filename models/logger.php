0a1bcd401fcce45da38c7be2b049bcfff7ba7127
Miguel Franco Galego <galegin@gmail.com>
2017-10-09 18:31:54 -0300

    Merge branch 'master' of https://github.com/galegin/igreja

--- models/logger.php
+++ models/logger.php
@@ -18,6 +18,7 @@ class Logger
 		$data = date('d.m.Y h:i:s');
 		$log = 
 <<<<<<< HEAD
+<<<<<<< HEAD
             "<log>\n" .
             "<data>$data<data/>\n" . 
             "<tipo>$tipo</tipo>\n" . 
@@ -25,12 +26,17 @@ class Logger
             "<metodo>$method</metodo>\n" . 
             "</log>\n\n" ;
 =======
+=======
+>>>>>>> 2f06a0a7c25700b2d65efd5924e69f94469c4ada
 			"<log>\n" .
 			"<data>$data</data>\n" .
 			"<tipo>$tipo</tipo>\n" .
 			"<message>$message</message>\n" .
 			"<metodo>$method</metodo>\n" . 
 			"</log>\n\n";
+<<<<<<< HEAD
+>>>>>>> 2f06a0a7c25700b2d65efd5924e69f94469c4ada
+=======
 >>>>>>> 2f06a0a7c25700b2d65efd5924e69f94469c4ada
 		$path = LOG_PATH . date("Y.m.d") . ".audit.xml";
 		error_log($log, 3, $path);
